6e1d1c2b4930a7dd7a3cbb0fe07392c9fb0d34be
Anna Julia <74986458+annajuliabreu@users.noreply.github.com>
2023-11-26 16:26:41 -0300

    login MCU att

--- workspace/components/MeuCalendarioUniversitario/login.jsx
+++ workspace/components/MeuCalendarioUniversitario/login.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
-import { Text, Image, View } from 'react-native';
-import formMCU from './Form/form';
+import { useState } from "react"
+import { View, Text, Image, TextInput, TouchableOpacity, } from 'react-native';
+import loginStyle from './loginStyle';
 
 const LoginMCU = () => {
+
+    // Estados para armazenar o e-mail e a senha inseridos pelo usuário
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Função para lidar com o clique no botão de login
+    const handleLogin = () => {
+        // Lógica de autenticação, fazer uma chamada a uma API
+        console.log('Email:', email);
+        console.log('Senha:', password);
+        // Lógica de autenticação aqui
+    };
+
     return (
-        <View>
-            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>
+        <View style={loginStyle.container}>
+            <View style={loginStyle.image}>
                 <Image
-                    style={styles.tinyLogo}
-                    source={require('@expo/snack-static/react-native-logo.png')}
+                    style={loginStyle.tinyLogo}
+                    source={{
+                        uri: 'https://github.com/annajuliabreu/mobile/blob/main/workspace/components/MeuCalendarioUniversitario/assets/Camada_1-2-2.png?raw=true',
+                    }}
                 />
-            </Text>
+            </View>
+
+            <View>
+                
+                <Text style={loginStyle.title}>Login</Text>
+
+                {/* Campo de entrada para o e-mail */}
+                <TextInput
+                    style={loginStyle.input}
+                    placeholder="E-mail"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+
+                {/* Campo de entrada para a senha */}
+                <TextInput
+                    style={loginStyle.input}
+                    placeholder="Senha"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+
+                {/* Botão de login */}
+                <TouchableOpacity style={loginStyle.button} onPress={handleLogin}>
+                    <Text style={loginStyle.buttonText}>Entrar</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
--- workspace/components/MeuCalendarioUniversitario/loginStyle.js
+++ workspace/components/MeuCalendarioUniversitario/loginStyle.js
@@ -1,37 +1,50 @@
 import { StyleSheet } from 'react-native';
 
 const loginStyle = StyleSheet.create({
+    image:{
+        paddingBottom: 16,
+    },
+    tinyLogo: {
+        width: 265,
+        height: 52,
+    },
+    header:{
+        fontSize: 30,
+        fontweight: 'bold',
+        paddingBottom: 20,
+        color: '#D81159',
+        fontWeight: 'bold',
+    },
     container: {
         flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#F283B6',
-        height: 667,
-        width: 375,
-    },
-    header: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        padding: 10,
-    },
-    input: {
+        alignItems: 'center',
+        paddingHorizontal: 16,
+        backgroundColor: '#F0FEFF',
+      },
+      title: {
+        fontSize: 24,
+        marginBottom: 16,
+      },
+      input: {
         height: 40,
-        width: 290,
-        borderColor: 'white',
-        borderRadius: 4,
-        borderWidth: 2,
-        paddingLeft: 10,
-        fontSize: 20,
-        margin: 4,
-    },
-    botoes: {
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        // backgroundColor: 'red',
-        marginTop: 5,
-        width: 300,
-    }
+        width: 250,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 12,
+        paddingHorizontal: 8,
+      },
+      button: {
+        backgroundColor: '#42946D',
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        borderRadius: 8,
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 16,
+      },
 })
 
 export default loginStyle;
